9c25c2fc0861811d9fa2c669ecc36444a7b3e171
AstaRom <astafev.roman.mail@gmail.com>
2020-06-23 16:42:35 +0500

    Create repository

---
+++ js/slider.jquery.js
@@ -0,0 +1,61 @@
+;(function($, window, undefined) {
+
+    $.fn.slider = function(prev, sig) {
+        return this.each(function(){
+            $container = $(this).children().eq(0);
+
+            if($container) {
+                var $fotos = $container.children();
+                var cantidad = $fotos.length;
+                var incremento = $fotos.outerWidth(true);
+                var enSlider = Math.floor($(this).width() / incremento);
+                var primerElemento = 1;
+                var estaMoviendo = false;
+            }
+
+            $container.css('width', (cantidad + enSlider) * incremento);
+            for(var i = 0; i < enSlider; i++) {
+                $container.append($fotos.eq(i).clone());
+            }
+
+            $(sig).click(function(e) {
+                e.preventDefault();
+
+                if(!estaMoviendo) {
+                   if(primerElemento > cantidad) {
+                        primerElemento = 2;
+                        $container.css('left', '0px');
+                    } else {
+                        primerElemento++;
+                    }
+                    estaMoviendo = true;
+                    $container.animate({
+                        left: '-=' + incremento,
+                    }, 'swing', function(){
+                        estaMoviendo = false;
+                    });
+                }
+            });
+
+            $(prev).click(function(e) {
+                e.preventDefault();
+
+                if(!estaMoviendo) {
+                    if(primerElemento == 1) {
+                        $container.css('left', cantidad * incremento * -1);
+                        primerElemento = cantidad;
+                    } else {
+                        primerElemento--;
+                    }
+                    estaMoviendo = true;
+                    $container.animate({
+                        left: '+=' + incremento,
+                    }, 'swing', function(){
+                        estaMoviendo = false;
+                    });
+                }
+            });
+        });
+    }
+
+})(jQuery, window)
